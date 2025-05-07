14d8148b4afb80f4952caf88a8d84039769ee4f1
ryanzomorrodi <ryanzomorrodi@gmail.com>
2025-05-07 12:44:23 -0500

    Make forward speed calc tests self-contained

--- tests/testthat/test_calculating_forward_speed.R
+++ tests/testthat/test_calculating_forward_speed.R
@@ -1,39 +1,15 @@
-library(tidyverse)
-library(lubridate)
-
-example_storms <- tribble(
-  ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
-  "Laura", "North Atlantic", ymd_hms("2020-08-27 03:00:00"), 29.10, -93.15,
-  "Laura", "North Atlantic", ymd_hms("2020-08-27 06:00:00"), 29.80, -93.30,
-  "Douglas", "Eastern Pacific", ymd_hms("2020-07-24 00:00:00"), 14.60, -138.00,
-  "Douglas", "Eastern Pacific", ymd_hms("2020-07-24 03:00:00"), 14.95, -138.74,
-  "Haishen", "Western Pacific", ymd_hms("2020-09-06 21:00:00"), 34.31, 128.97,
-  "Haishen", "Western Pacific", ymd_hms("2020-09-07 00:00:00"), 35.50, 128.90,
-  "Amphan", "Northern Indian", ymd_hms("2020-05-20 00:00:00"), 19.20, 87.40,
-  "Amphan", "Northern Indian", ymd_hms("2020-05-20 03:00:00"), 19.79, 87.66,
-  "Belna", "Southern Indian", ymd_hms("2019-12-09 12:00:00"), -12.40, 46.50,
-  "Belna", "Southern Indian", ymd_hms("2019-12-09 15:00:00"), -12.63, 46.41,
-  "Harold", "Southern Pacific", ymd_hms("2020-04-03 12:00:00"), -12.70, 163.00,
-  "Harold", "Southern Pacific", ymd_hms("2020-04-03 15:00:00"), -13.71, 163.41,
-  "Not Named", "Southern Atlantic", ymd_hms("2004-03-28 06:00:00"), -29.00, -49.60,
-  "Not Named", "Southern Atlantic", ymd_hms("2004-03-28 09:00:00"), -28.77, -49.93,
-  "Ophelia", "crossed prime meridian", ymd_hms("2005-09-22 12:00:00"), 65.60, -1.00,
-  "Ophelia", "crossed prime meridian", ymd_hms("2005-09-22 18:00:00"), 66.60, 1.90,
-  "Harold", "crossed international date line", ymd_hms("2020-04-08 06:00:00"), -20.11, 179.70,
-  "Harold", "crossed international date line", ymd_hms("2020-04-08 12:00:00"), -20.60, -178.1,
-  ## IBTrACS continues above 180 when tracking storms like this, it seems (i.e., won't change hemisphere convention mid-storm)
-  "Harold", "crossed international date line IBTrACS", ymd_hms("2020-04-08 06:00:00"), -20.11, 179.70,
-  "Harold", "crossed international date line IBTrACS", ymd_hms("2020-04-08 12:00:00"), -20.60, 181.90
-)
-
 # Distance estimates compared with those made through NOAA widget, available
 # at https://www.nhc.noaa.gov/gccalc.shtml
 
 ## Estimates of storm forward speed are also available in IBTrACS
 
 test_that("Forward speed calculation correct for North Atlantic basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "North Atlantic")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Laura", "North Atlantic", lubridate::ymd_hms("2020-08-27 03:00:00"), 29.10, -93.15,
+    "Laura", "North Atlantic", lubridate::ymd_hms("2020-08-27 06:00:00"), 29.80, -93.30
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -59,8 +35,12 @@ test_that("Forward speed calculation correct for North Atlantic basin storm", {
 })
 
 test_that("Forward speed calculation correct for Eastern Pacific basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Eastern Pacific")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Douglas", "Eastern Pacific", lubridate::ymd_hms("2020-07-24 00:00:00"), 14.60, -138.00,
+    "Douglas", "Eastern Pacific", lubridate::ymd_hms("2020-07-24 03:00:00"), 14.95, -138.74
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -78,8 +58,12 @@ test_that("Forward speed calculation correct for Eastern Pacific basin storm", {
 })
 
 test_that("Forward speed calculation correct for Western Pacific basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Western Pacific")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Haishen", "Western Pacific", lubridate::ymd_hms("2020-09-06 21:00:00"), 34.31, 128.97,
+    "Haishen", "Western Pacific", lubridate::ymd_hms("2020-09-07 00:00:00"), 35.50, 128.90
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -97,8 +81,12 @@ test_that("Forward speed calculation correct for Western Pacific basin storm", {
 })
 
 test_that("Forward speed calculation correct for Northern Indian basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Northern Indian")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Amphan", "Northern Indian", lubridate::ymd_hms("2020-05-20 00:00:00"), 19.20, 87.40,
+    "Amphan", "Northern Indian", lubridate::ymd_hms("2020-05-20 03:00:00"), 19.79, 87.66
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -116,8 +104,12 @@ test_that("Forward speed calculation correct for Northern Indian basin storm", {
 })
 
 test_that("Forward speed calculation correct for Southern Indian basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Southern Indian")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Belna", "Southern Indian", lubridate::ymd_hms("2019-12-09 12:00:00"), -12.40, 46.50,
+    "Belna", "Southern Indian", lubridate::ymd_hms("2019-12-09 15:00:00"), -12.63, 46.41
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -135,8 +127,12 @@ test_that("Forward speed calculation correct for Southern Indian basin storm", {
 })
 
 test_that("Forward speed calculation correct for Southern Pacific basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Southern Pacific")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Harold", "Southern Pacific", lubridate::ymd_hms("2020-04-03 12:00:00"), -12.70, 163.00,
+    "Harold", "Southern Pacific", lubridate::ymd_hms("2020-04-03 15:00:00"), -13.71, 163.41
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -154,8 +150,12 @@ test_that("Forward speed calculation correct for Southern Pacific basin storm", 
 })
 
 test_that("Forward speed calculation correct for Southern Atlantic basin storm", {
-  storm <- example_storms %>%
-    filter(basin == "Southern Atlantic")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Not Named", "Southern Atlantic", lubridate::ymd_hms("2004-03-28 06:00:00"), -29.00, -49.60,
+    "Not Named", "Southern Atlantic", lubridate::ymd_hms("2004-03-28 09:00:00"), -28.77, -49.93
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -173,8 +173,12 @@ test_that("Forward speed calculation correct for Southern Atlantic basin storm",
 })
 
 test_that("Forward speed calculation correct when crossing prime meridian", {
-  storm <- example_storms %>%
-    filter(basin == "crossed prime meridian")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Ophelia", "crossed prime meridian", lubridate::ymd_hms("2005-09-22 12:00:00"), 65.60, -1.00,
+    "Ophelia", "crossed prime meridian", lubridate::ymd_hms("2005-09-22 18:00:00"), 66.60, 1.90
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -192,8 +196,12 @@ test_that("Forward speed calculation correct when crossing prime meridian", {
 })
 
 test_that("Forward speed calculation correct when crossing international date line", {
-  storm <- example_storms %>%
-    filter(basin == "crossed international date line")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Harold", "crossed international date line", lubridate::ymd_hms("2020-04-08 06:00:00"), -20.11, 179.70,
+    "Harold", "crossed international date line", lubridate::ymd_hms("2020-04-08 12:00:00"), -20.60, -178.1
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
@@ -211,8 +219,12 @@ test_that("Forward speed calculation correct when crossing international date li
 })
 
 test_that("Forward speed calculation correct when crossing international date line under IBTrACS conventions", {
-  storm <- example_storms %>%
-    filter(basin == "crossed international date line IBTrACS")
+  storm <- tibble::tribble(
+    ~ storm_name, ~ basin, ~ time, ~ lat, ~ lon,
+    "Harold", "crossed international date line IBTrACS", lubridate::ymd_hms("2020-04-08 06:00:00"), -20.11, 179.70,
+    "Harold", "crossed international date line IBTrACS", lubridate::ymd_hms("2020-04-08 12:00:00"), -20.60, 181.90
+  )
+
   dist <- latlon_to_km(tclat_1 = storm$lat[1],
                        tclon_1 = storm$lon[1],
                        tclat_2 = storm$lat[2],
